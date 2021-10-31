3485b0d46bb3d27635837b4232745ff8c190d75b
jing <jing@caravan.coop>
2021-10-31 14:42:55 -0400

    add missing module, adjust visual

--- src/app/timer/customize-timer/customize-timer.module.ts
+++ src/app/timer/customize-timer/customize-timer.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { CustomizeTimerPageRoutingModule } from './customize-timer-routing.module';
@@ -6,6 +7,7 @@ import { CustomizeTimerPage } from './customize-timer.page';
 
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule,
     SharedModule,
     CustomizeTimerPageRoutingModule
